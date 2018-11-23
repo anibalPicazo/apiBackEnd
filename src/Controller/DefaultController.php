a4778f24ad95673552984f2d7c1d0101b7690129
Desarrollo5 <anibal.picazo@hotmail.com>
2018-11-23 11:54:46 +0100

    envio de usuario y contraseña

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use App\Entity\Video;
 use App\Entity\Comment;
 use App\Service\Helpers;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -15,8 +16,23 @@ class DefaultController extends Controller
     
      
 
-     public function prueba(){
-         echo"hello";
+     public function loginAction(Request $request){
+   
+
+         $helpers = $this->get( Helpers::class);
+         $json = $request -> get("json",null);
+         
+         if($json != null)
+         {
+             //decodifica un json para poderlo utilizar.
+             $params=  json_decode($json);
+             $email = (isset($params -> email)) ? $params -> email:null;
+             $password = (isset($params -> password)) ? $params -> password:null;
+             var_dump($email);
+             var_dump($password);
+         }else{
+             echo("Introduzca el email y la contraseña");
+         }
          die();
      }
 
