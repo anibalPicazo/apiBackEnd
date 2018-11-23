c914337e3c333849bcd74a5a999e4d0f10eddce8
Desarrollo5 <anibal.picazo@hotmail.com>
2018-11-23 12:37:01 +0100

    Validacion email

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,6 +10,8 @@ use App\Entity\Comment;
 use App\Service\Helpers;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class DefaultController extends Controller
 {
     
@@ -28,8 +30,19 @@ class DefaultController extends Controller
              $params=  json_decode($json);
              $email = (isset($params -> email)) ? $params -> email:null;
              $password = (isset($params -> password)) ? $params -> password:null;
-             var_dump($email);
-             var_dump($password);
+             $emailContraint= new Assert\Email();
+             $emailContraint -> message= "This email is not valid";
+             $validate_email = $this-> get("validator") -> validate ($email,$emailContraint);
+            if( count($validate_email)==0 && $password != null){
+
+                echo "Data success";
+
+
+
+            }else {
+                echo "data incorrect";
+            }
+             
          }else{
              echo("Introduzca el email y la contraseña");
          }
